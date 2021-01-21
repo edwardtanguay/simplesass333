e766ed453dce3e62572e07694038f9dce5090579
Edward Tanguay (wk) <edward@tanguay.info>
2021-01-21 17:00:42 +0100

    finished

---
+++ README.md
@@ -0,0 +1,6 @@
+# simplesass
+
+Simplest SASS site with static HTML
+
+## characteristics
+
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const sassMiddleware = require('node-sass-middleware');
+const sassMiddleware = require('node-sass-middleware')
 const path = require('path');
 const app = express();
-app.use(sassMiddleware({
-	src: __dirname,
-	dest: path.join(__dirname, 'public'),
+app.use('/styles', sassMiddleware({
+	src: path.join(__dirname, 'sass'),
+	dest: path.join(__dirname, 'public/styles'),
 	debug: true,
-	indentedSyntax: false,
-	outputStyle: 'compressed',
-	prefix: '/prefix'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+	indentedSyntax: false
 }));
+
+const port = 3002;
+
 app.use('/', express.static(path.join(__dirname, './public')));
+
+app.listen(port, () => {
+	console.log(`app listening at: http://localhost:${port}`);
+});
